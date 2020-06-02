d6ae2131a60959ed08adda22da9a2ed4459ab9d1
Manfredo Stagno d'Alcontres <manfredo@stagno.it>
2020-06-03 00:13:40 +0200

    Started Work on Hierarchy

---
+++ Hierarchy_Project/Assets/Scripts/Hierarchy/Character.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HP.Hierarchy
+{
+    public class Character
+    {        
+        HierarchyController hierarchyController;
+        
+        Character boss;
+        List<Character> underlings = new List<Character>();
+
+        float treasure;
+        float power;
+        float authority;
+
+        public Character(HierarchyController controller)
+        {
+            hierarchyController = controller;
+        }
+
+        public void AddBoss(Character newBoss)
+        {
+            boss = newBoss;
+        }
+
+        public void AddUnderling(Character newUnderling)
+        {
+            underlings.Add(newUnderling);
+        }
+    }    
+}
---
+++ Hierarchy_Project/Assets/Scripts/Hierarchy/HierarchyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HP.Hierarchy
+{
+    public class HierarchyController : MonoBehaviour
+    {
+        List<Character> characters = new List<Character>();
+
+        private void AddCharacter(Character character)
+        {
+            characters.Add(character);
+        }
+
+        private void CreateNewCharacter()
+        {
+            Character character = new Character(this);
+            AddCharacter(character);
+        }
+    }
+}
